4bc49aa9caa97274a10c6743c06313e3788c9c94
Liya-Getachew <113055994+Liya-Getachew@users.noreply.github.com>
2022-10-15 23:26:02 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,28 @@
+// Created by: liya getachew 
+// Created on: Oct 15 2022
+//
+// This program accepts user input
+
+using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        // This function accepts user input
+        int sideLengthOfSquare;
+      
+        Console.WriteLine ("This program calculates the perimeter of a square in meters.");
+        Console.WriteLine("");
+
+        Console.Write("Enter side length of square (m): ");
+        sideLengthOfSquare = Convert.ToInt32(Console.ReadLine());
+
+        int perimeter = sideLengthOfSquare * 4;
+
+        Console.WriteLine("");
+        Console.WriteLine("Perimeter = " + perimeter + " m.");
+
+        Console.WriteLine("\nDone");
+    }
+}
